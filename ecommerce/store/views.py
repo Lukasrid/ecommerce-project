fa0411adba1f00ff4dde7f75e40616c5a7bb0553
Lukasrid <lukas.ridstrom@gmail.com>
2023-08-30 08:14:26 +0000

    linked up css

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -6,10 +6,10 @@ def store(request):
     context = {}
     return render(request, 'store/store.html', context)
 
-    def cart(request):
+def cart(request):
     context = {}
     return render(request, 'store/cart.html', context)
 
-    def checkout(request):
+def checkout(request):
     context = {}
     return render(request, 'store/checkout.html', context)
