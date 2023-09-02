e45052ab7a370c0819434bdde47b8b8cb8b2d23f
Lukasrid <lukas.ridstrom@gmail.com>
2023-09-02 08:21:22 +0000

    Cart total is displayed on the cart in top right hand corner for all pages

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,9 +7,22 @@ from .models import *
 # Create your views here.
 
 def store(request):
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
+	else:
+		#Create empty cart for now for non-logged in user
+		items = []
+		order = {'get_cart_total':0, 'get_cart_items':0}
+		cartItems = order['get_cart_items']
+
 	books = Book.objects.all()
-	context = {'books':books}
+	context = {'books':books, 'cartItems':cartItems}
 	return render(request, 'store/store.html', context)
+
 
 def cart(request):
 
@@ -17,12 +30,14 @@ def cart(request):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0}
+		cartItems = order['get_cart_items']
 
-	context = {'items':items, 'order':order}
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -30,12 +45,14 @@ def checkout(request):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items
 	else:
 		#Create empty cart for now for non-logged in user
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0}
+		cartItems = order['get_cart_items']
 
-	context = {'items':items, 'order':order}
+	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
