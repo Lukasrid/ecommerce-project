32e0bb60b3de73f3d3bbf60fb73f533246ba532a
Lukasrid <lukas.ridstrom@gmail.com>
2023-09-02 08:52:14 +0000

    Hide shipping information on checkout page if product is digital

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -12,24 +12,24 @@ class Customer(models.Model):
 		return self.name
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=200)
-    price = models.FloatField()
-    digital = models.BooleanField(default=False,null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+	title = models.CharField(max_length=200)
+	author = models.CharField(max_length=200)
+	price = models.FloatField()
+	digital = models.BooleanField(default=False,null=True, blank=True)
+	image = models.ImageField(null=True, blank=True)
 
-    def __str__(self):
-        return self.title
+	def __str__(self):
+		return self.title
 
-    @property
-    def imageURL(self):
-        try:
-            url = self.image.url
-        except:
-            url = ''
-        return url
+	@property
+	def imageURL(self):
+		try:
+			url = self.image.url
+		except:
+			url = ''
+		return url
 
-      
+	  
 
 class Order(models.Model):
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -39,6 +39,15 @@ class Order(models.Model):
 
 	def __str__(self):
 		return str(self.id)
+
+	@property
+	def shipping(self):
+		shipping = False
+		orderitems = self.orderitem_set.all()
+		for i in orderitems:
+			if i.book.digital == False:
+				shipping = True
+		return shipping
 
 	@property
 	def get_cart_total(self):
