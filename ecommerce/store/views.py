8d5478cd3d2d89812ab249cf6085ff76889728bb
Lukasrid <lukas.ridstrom@gmail.com>
2023-09-02 12:02:49 +0000

    Trying to figure out how to grab users name and email to fill into form automatically

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -79,3 +79,31 @@ def updateItem(request):
 		orderItem.delete()
 
 	return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+	transaction_id = datetime.datetime.now().timestamp()
+	data = json.loads(request.body)
+
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		total = float(data['form']['total'])
+		order.transaction_id = transaction_id
+
+		if total == order.get_cart_total:
+			order.complete = True
+		order.save()
+
+		if order.shipping == True:
+			ShippingAddress.objects.create(
+			customer=customer,
+			order=order,
+			address=data['shipping']['address'],
+			city=data['shipping']['city'],
+			county=data['shipping']['county'],
+			postcode=data['shipping']['postcode'],
+			)
+	else:
+		print('User is not logged in')
+
+	return JsonResponse('Payment submitted..', safe=False)
