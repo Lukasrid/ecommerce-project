58a7853758180b0306d8bd912897dbee3650a31a
Lukasrid <lukas.ridstrom@gmail.com>
2023-09-04 09:39:12 +0000

    Start building guest checkout functionality

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -8,13 +8,38 @@ for (i = 0; i < updateBtns.length; i++) {
 
 		console.log('USER:', user)
 		if (user == 'AnonymousUser'){
-			console.log('User is not authenticated')
+			addCookieItem(bookId, action)
 			
 		}else{
 			updateUserOrder(bookId, action)
 		}
 
 	})
+}
+
+function addCookieItem(bookId, action){
+	console.log('User is not authenticated!!!')
+
+	if (action == 'add'){
+		if (cart[bookId] == undefined){
+		cart[bookId] = {'quantity':1}
+
+		}else{
+			cart[bookId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[bookId]['quantity'] -= 1
+
+		if (cart[bookId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[bookId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	location.reload()
 }
 
 function updateUserOrder(bookId, action){
